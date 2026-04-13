bfea6dcb2b0ea9b2a957e5e1c08ace760ba044bb
Aymard <waufototzo@gmail.com>
2026-04-14 01:49:05 +0200

    feat(infrastructure): init spring boot project with clean architecture structure

---
+++ src/test/java/com/edusaas/EdusaasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edusaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdusaasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
